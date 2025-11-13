a927a28b42f1a390fc0466787a20334d5e44e46f
sabrin16 <sabrin.salem@hotmail.se>
2025-11-13 21:07:01 +0100

    prod

---
+++ UmbracoProject/ViewModels/CallbackFormViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace UmbracoProject.ViewModels;
+
+public class CallbackFormViewModel
+{
+    [Required(ErrorMessage = "Name is required")]
+    [Display(Name = "Name")]
+    public string Name { get; set; } = null!;
+
+    [Required(ErrorMessage = "Email is required")]
+    [Display(Name = "Email address")]
+    [RegularExpression(@"^(([^<>()\[\]\\.,;:\s@\""]+(\.[^<>()\]\\.,;:\s@\""]+)*)|("".+""))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$", ErrorMessage = "Invalid email address")]
+    public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "Phone is required")]
+    [Display(Name = "Phone")]
+    public string Phone { get; set; } = null!;
+
+    [Required(ErrorMessage = "Please select an option")]
+    public string SelectedOption { get; set; } = null!;
+
+    [BindNever]
+    public IEnumerable<String> Options { get; set; } = [];
+}
---
+++ UmbracoProject/wwwroot/js/form.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("submit", function (e) {
+    if (e.target.matches("form")) {
+        sessionStorage.setItem("scrolly", window.scrollY);
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const scrolly = sessionStorage.getItem("scrolly");
+    if (scrolly) {
+        window.scrollTo({ top: parseInt(scrolly, 10), behavior: "instant" });
+        sessionStorage.removeItem("scrolly");
+    }
+});
